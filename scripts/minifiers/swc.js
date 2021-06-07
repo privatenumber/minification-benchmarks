f505570676d887dae7629973d0ad2ef947bbeac3
Hiroki Osame <hiroki.osame@gmail.com>
2021-06-06 23:17:18 -0400

    style: remove paren

--- scripts/minifiers/swc.js
+++ scripts/minifiers/swc.js
@@ -1,10 +1,10 @@
 import swc from '@swc/core';
 
 export default async ({ code }) => {
-	const minified = (await swc.transform(code, {
+	const minified = await swc.transform(code, {
 		minify: true,
 		sourceMaps: false,
 		swcrc: false,
-	}));
+	});
 	return minified.code;
 };
