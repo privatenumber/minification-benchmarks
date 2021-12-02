d2cb7b15ebeba00249ea63be213c2dfb050d0213
Hiroki Osame <hiroki.osame@gmail.com>
2021-12-02 12:17:17 -0500

    wip

--- scripts/benchmark/run-test.ts
+++ scripts/benchmark/run-test.ts
@@ -7,6 +7,21 @@ export const requireString = (code: string) => {
 	return fsRequire('/index');
 };
 
+function disablelogs() {
+	const stdoutWrite = process.stdout.write;
+	const stderrWrite = process.stderr.write;
+
+	// @ts-expect-error
+	process.stdout.write = () => {};
+	// @ts-expect-error
+	process.stderr.write = () => {};
+
+	return function reenable() {
+		process.stdout.write = stdoutWrite;
+		process.stderr.write = stderrWrite;
+	};
+}
+
 export async function runTest(testPath: string, code: string) {
 	// eslint-disable-next-line node/global-require,@typescript-eslint/no-var-requires
 	const { preprocess, run } = require(testPath).default;
@@ -15,14 +30,18 @@ export async function runTest(testPath: string, code: string) {
 		code = preprocess(code);
 	}
 
+	const reenableLogs = disablelogs();
+	
 	try {
 		await run(requireString(code));
-	} catch (error) {
+	} catch (error) {		
 		if (error instanceof Error) {
 			// eslint-disable-next-line unicorn/prefer-type-error
 			throw new Error(`Invalid output: ${error.name}`);
 		}
 
 		throw new Error('Invalid output');
+	} finally {
+		reenableLogs();
 	}
 }
