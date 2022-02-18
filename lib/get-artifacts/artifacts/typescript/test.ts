83ed573d47d7de941539dea69f6068fcdc8353eb
Hiroki Osame <hiroki.osame@gmail.com>
2022-02-18 16:16:47 -0500

    style: quotes

--- lib/get-artifacts/artifacts/typescript/test.ts
+++ lib/get-artifacts/artifacts/typescript/test.ts
@@ -4,8 +4,13 @@ import { defineTest } from '../..';
 
 export default defineTest({
 	run(ts: typeof typescript) {
-		const source = "let x: () => string = () => 'string'";
-		const result = ts.transpileModule(source, { compilerOptions: { target: ts.ScriptTarget.ES3 } });
-		assert.strictEqual(result.outputText, "var x = function () { return 'string'; };\n");
+		const source = 'let x: () => string = () => "string"';
+		const result = ts.transpileModule(source, {
+			compilerOptions: {
+				target: ts.ScriptTarget.ES3,
+			},
+		});
+
+		assert.strictEqual(result.outputText, 'var x = function () { return "string"; };\n');
 	},
 });
