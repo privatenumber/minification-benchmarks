41a1f0f0bd286473fbef473871289ab691355f0f
hiroki osame <hiroki.osame@gmail.com>
2022-03-07 23:06:04 -0500

    feat: add "typescript" as artifact (#103)
    Co-authored-by: Pig Fang <g-plane@hotmail.com>
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ lib/get-artifacts/artifacts/typescript/test.ts
@@ -0,0 +1,16 @@
+import assert from 'assert';
+import type typescript from 'typescript';
+import { defineTest } from '../..';
+
+export default defineTest({
+	run(ts: typeof typescript) {
+		const source = 'let x: () => string = () => "string"';
+		const result = ts.transpileModule(source, {
+			compilerOptions: {
+				target: ts.ScriptTarget.ES3,
+			},
+		});
+
+		assert.strictEqual(result.outputText, 'var x = function () { return "string"; };\n');
+	},
+});
