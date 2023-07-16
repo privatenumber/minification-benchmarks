594022006a4fc6b1d99bbc066592604f09b75c8e
hiroki osame <hiroki.osame@gmail.com>
2023-07-16 08:32:36 -0400

    feat: add bun (#373)
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ lib/minifiers/bun.ts
@@ -0,0 +1,19 @@
+import { spawn } from 'child_process';
+import type { Stream } from 'stream';
+import { minifier } from '../types';
+
+const stream2buffer = (
+	stream: Stream,
+) => new Promise<Buffer>((resolve, reject) => {
+	const bufferChunks = new Array<any>();
+
+	stream.on('error', reject);
+	stream.on('data', chunk => bufferChunks.push(chunk));
+	stream.on('end', () => resolve(Buffer.concat(bufferChunks)));
+});
+
+export default minifier(async ({ filePath }) => {
+	const minify = spawn('bun', ['build', '--no-bundle', '--minify', filePath]);
+	const minified = await stream2buffer(minify.stdout);
+	return minified.toString();
+});
