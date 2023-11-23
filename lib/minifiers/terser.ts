c93430b4e6dd0826636a98a532fb317a973b747b
Guilherme Simoes <guilherme.rdems@gmail.com>
2023-11-23 15:32:40 +0000

    perf(Terser): set minify target to ES2018 (#443)

--- lib/minifiers/terser.ts
+++ lib/minifiers/terser.ts
@@ -3,6 +3,7 @@ import { minifier } from '../types';
 
 export default minifier(async ({ code }) => {
 	const minified = await minify(code, {
+		ecma: 2018,
 		sourceMap: false,
 		output: {
 			comments: false,
