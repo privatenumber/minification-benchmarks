d7d4758fba626dfbb64d7525b1587d06a2895c48
Hiroki Osame <hiroki.osame@gmail.com>
2024-01-18 12:18:54 +0900

    wip

--- packages/minifiers/minifiers/bun.ts
+++ packages/minifiers/minifiers/bun.ts
@@ -1,9 +1,17 @@
+import path from 'path';
 import { spawn } from 'child_process';
 import { streamToBuffer } from '@minification-benchmarks/utils/stream-to-buffer';
 import { minifier } from '../types.js';
 
 export default minifier(async ({ filePath }) => {
-	const minify = spawn('bun', ['build', '--no-bundle', '--minify', filePath]);
+	const minify = spawn('bun', ['build', '--no-bundle', '--minify', filePath], {
+		env: {
+			PATH: [
+				path.join(__dirname, '../node_modules/.bin'),
+				process.env.PATH,
+			].join(':'),
+		},
+	});
 	const minified = await streamToBuffer(minify.stdout);
 	return minified.toString();
 });
