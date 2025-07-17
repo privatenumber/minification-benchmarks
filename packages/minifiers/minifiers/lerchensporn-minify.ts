4d1031517764ff708947d8335aab18d202b94c66
Lerchensporn <>
2025-07-17 18:25:27 +0200

    Add Lerchensporn/minify

---
+++ packages/minifiers/minifiers/lerchensporn-minify.ts
@@ -0,0 +1,29 @@
+import { spawn } from 'child_process';
+import { collectStream } from '@minification-benchmarks/utils/collect-stream';
+import { createMinifier } from '../utils/create-minifier.js';
+
+export default createMinifier(
+	'@lerchensporn/minify',
+	{
+		default: async ({ code }) => {
+			const executable = new URL('../minify', import.meta.url).pathname;
+			const minify = spawn(executable, ['js', '-']);
+			minify.stdin.end(code);
+
+			const [error, minified] = await Promise.all([
+				collectStream(minify.stderr),
+				collectStream(minify.stdout),
+			]);
+
+			if (error) {
+				const [message, ...stackLines] = error.split('\n');
+				const errorObject = new Error(message);
+				errorObject.stack = stackLines.join('\n');
+				throw errorObject;
+			}
+
+			return minified;
+		},
+	},
+	{version: '2'}
+);
