cd4dc1d235e904aa77db11f2a6a943c78429c633
Jumping-Beaver <829649+Jumping-Beaver@users.noreply.github.com>
2025-07-29 15:29:19 +0200

    feat: add minifier "cminify" (#781)
    Co-authored-by: Lerchensporn <>
    Co-authored-by: Hiroki Osame <hiroki.osame@gmail.com>

---
+++ packages/minifiers/minifiers/cminify.ts
@@ -0,0 +1,28 @@
+import { spawn } from 'child_process';
+import { collectStream } from '@minification-benchmarks/utils/collect-stream';
+import { createMinifier } from '../utils/create-minifier.js';
+
+export default createMinifier(
+	'@cminify/cminify-linux-x64',
+	{
+		default: async ({ code }) => {
+			const executable = new URL('../node_modules/.bin/cminify', import.meta.url).pathname;
+			const minify = spawn(executable, ['js', '-']);
+			minify.stdin.end(code);
+
+			const [error, minified] = await Promise.all([
+				collectStream(minify.stderr),
+				collectStream(minify.stdout),
+			]);
+
+			if (error) {
+				const [message, ...stackLines] = error.split('\n');
+				const errorObject = new Error(message);
+				errorObject.stack = stackLines.join('\n');
+				throw errorObject;
+			}
+
+			return minified;
+		},
+	},
+);
