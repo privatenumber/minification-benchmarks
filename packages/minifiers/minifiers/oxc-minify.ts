0b38ded4e0d8337ad040ae1a536c87d26127a297
Boshen <boshenc@gmail.com>
2025-11-18 22:44:01 +0800

    feat: use `minifySync` instead of async for `oxc-minify`
    Using the sync version because the async version spawns a real thread,
    which is slow for single threaded js benchmarks.
    
    The purpose of this PR is to observe performance difference between the
    async and sync version, feel free to reject if it does not meet the
    requirements of this project.

--- packages/minifiers/minifiers/oxc-minify.ts
+++ packages/minifiers/minifiers/oxc-minify.ts
@@ -1,11 +1,12 @@
-import { minify } from 'oxc-minify';
+import { minifySync } from 'oxc-minify';
 import { createMinifier } from '../utils/create-minifier.js';
 
 export default createMinifier(
 	'oxc-minify',
 	{
 		default: async ({ filePath, code }) => {
-			const minified = await minify(filePath, code);
+			// Using the sync version because the async version spawns a real thread, which is slow for js benchmarks.
+			const minified = minifySync(filePath, code);
 			return minified.code;
 		},
 	},
